79ef8b5a58cb305344d998b72f0c4860dd0682ee
Onabanjo Titus <titus.onabanjo@arikair.com>
2020-12-03 18:57:23 +0100

    changed example

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -13,11 +13,11 @@ public class Arithmetic {
             int sum = x+y;
             int product = x*y;
             int difference = x-y;
-            int qoutient = x/y;
+            int quotient = x/y;
 
             System.out.printf("The sum of the numbers: %d, %d. is %d%n", x,y,sum);
             System.out.printf("The product of the numbers: %d, %d. is %d%n", x,y,product);
             System.out.printf("The difference of the numbers: %d, %d. is %d%n", x,y,difference);
-            System.out.printf("The qoutient of the numbers: %d, %d. is %d%n", x,y,qoutient);
+            System.out.printf("The quotient of the numbers: %d, %d. is %d%n", x,y,quotient);
         }
 }
